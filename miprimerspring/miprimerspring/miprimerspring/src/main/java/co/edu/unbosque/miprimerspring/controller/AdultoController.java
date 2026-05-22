2c89c2fa5eef329aa93485e4504f930b845a9845
Juan Carlos <juankamome95@gmail.com>
2026-05-22 12:36:30 -0500

    a
    aa

--- miprimerspring/miprimerspring/miprimerspring/src/main/java/co/edu/unbosque/miprimerspring/controller/AdultoController.java
+++ miprimerspring/miprimerspring/miprimerspring/src/main/java/co/edu/unbosque/miprimerspring/controller/AdultoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import co.edu.unbosque.miprimerspring.dto.JokeDTO;
 
 @RestController
 @RequestMapping("/adulto")
