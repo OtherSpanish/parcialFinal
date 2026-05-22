5d0c0b0c52d91f83177e8afcd586f86e8fd8ce81
Juan Carlos <juankamome95@gmail.com>
2026-05-22 12:35:59 -0500

    Controller adulto y ninyo

--- miprimerspring/miprimerspring/miprimerspring/src/main/java/co/edu/unbosque/miprimerspring/controller/AdultoController.java
+++ miprimerspring/miprimerspring/miprimerspring/src/main/java/co/edu/unbosque/miprimerspring/controller/AdultoController.java
@@ -1,15 +1,33 @@
 package co.edu.unbosque.miprimerspring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/adulto")
 @CrossOrigin(origins = { "http://localhost:4200", "*" })
 public class AdultoController {
-	
-	
-	
-	
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	/**
+	 * Retorna un chiste de cualquier tipo, sin restricciones.
+	 * Solo accesible para usuarios con rol ADULTO.
+	 */
+	@GetMapping("/chiste")
+	public ResponseEntity<JokeDTO> getChiste() {
+
+		String url = "https://v2.jokeapi.dev/joke/Any";
+
+		JokeDTO joke = restTemplate.getForObject(url, JokeDTO.class);
+
+		return ResponseEntity.ok(joke);
+	}
 }
